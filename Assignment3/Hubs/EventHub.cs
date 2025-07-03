c782c780633d8b343e50b8ba1d105af7f94e11a2
anh.nguyen49 <anh.nguyen49@ntq-solution.com.vn>
2025-07-03 09:37:52 +0700

    revert eventhub

---
+++ Assignment3/Hubs/EventHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Assignment3.Hubs
+{
+	[Authorize]
+	public class EventHub : Hub
+	{
+		public async Task JoinEventGroup(string eventId)
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, $"Event_{eventId}");
+		}
+
+		public async Task LeaveEventGroup(string eventId)
+		{
+			await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"Event_{eventId}");
+		}
+
+		public async Task SendEventUpdate(string eventId, string message)
+		{
+			await Clients.Group($"Event_{eventId}").SendAsync("EventUpdated", message);
+		}
+
+		public async Task SendAttendeeUpdate(string eventId, string message)
+		{
+			await Clients.Group($"Event_{eventId}").SendAsync("AttendeeUpdated", message);
+		}
+
+		public override async Task OnConnectedAsync()
+		{
+			await Clients.Caller.SendAsync("Connected", "You are connected to the event hub");
+			await base.OnConnectedAsync();
+		}
+
+		public override async Task OnDisconnectedAsync(Exception? exception)
+		{
+			await base.OnDisconnectedAsync(exception);
+		}
+	}
+}
